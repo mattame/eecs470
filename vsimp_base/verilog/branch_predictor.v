3fc8585121c725eef1bd395a57b1682d497c92f2
mepilla <mepilla@umich.edu>
2013-04-10 20:13:44 -0300

    Update branch_predictor.v

--- vsimp_base/verilog/branch_predictor.v
+++ vsimp_base/verilog/branch_predictor.v
@@ -17,15 +17,19 @@ output reg [4:0]pht_index_out;
 //----------------internal-----------
 reg [4:0]pht;
 reg [2:0]ghr;
+reg [2:0]new_ghr;
 reg [4:0]pc_bits;
 reg [4:0]ghr_bits;
 reg [4:0]pht_index;
 reg [5:0]inst_opcode1
 reg [5:0]inst_opcode2;
 
+
 inst_opcode1 = instruction[63:58];
 inst_opcode2 = instruction[31:26];
 
+always@*
+begin
 //decode instruction to check if branch
 if (inst_opcode1 ==  BR_INST   || inst_opcode1 == BSR_INST ||  
     inst_opcode1 ==  BLBC_INST || inst_opcode1 == BEQ_INST || 
@@ -36,11 +40,12 @@ if (inst_opcode1 ==  BR_INST   || inst_opcode1 == BSR_INST ||
   isBranch = 1;
 else
   isBranch = 0;
+end 
 
 //init pht to not taken. 0 means not taken, 1 means taken. 
 pht = 32'b0;
 
-alwasys @*
+always@*
 begin
 //for use with the xor
 pc_bits = {pc[3:2], 3'b0};
@@ -57,12 +62,25 @@ pht_index_out = pht_index;
 end
 
 
-always@
-ghr = ghr<<1;
+
+assign new_ghr = ghr<<1;
+always@(posedge clk)
+begin
+  if(reset)
+  begin
+    ghr <= 3'b0;
+  end
+
+  else
+  begin
+    ghr <= new_ghr;
+  end
+end
 
 //assume that unconditional branches don't count in the global branch history register (ghr)
 if (inst_opcode1 != BR_INST && inst_opcode1 != BSR_INST)
-  ghr[0] = prediction;
+  ghr[0] = result;
+end
 
 if(result != pht[pht_index_in])
   pht[pht_index_in] = result;

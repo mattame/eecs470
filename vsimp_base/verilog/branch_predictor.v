631f629edebddccbcebb6764a65585b9660feeed
mepilla <mepilla@umich.edu>
2013-04-10 19:55:10 -0300

    Update branch_predictor.v

--- vsimp_base/verilog/branch_predictor.v
+++ vsimp_base/verilog/branch_predictor.v
@@ -1,12 +1,13 @@
-module branch_predictor (clock, reset, pc, isBranch, result, pht_index_in, prediction, pht_index_out)
+module branch_predictor (clock, reset, pc, instruction, isBranch, result, pht_index_in, prediction, pht_index_out)
 
 //----------------inputs------------
 input wire clock;
 input wire reset;
 
 input reg [31:0]pc;
-input reg isBranch;
+input wire isBranch;
 input reg [4:0]pht_index_in;
+input reg [63:0]instruction;
 
 
 //----------------outputs-----------
@@ -19,7 +20,22 @@ reg [2:0]ghr;
 reg [4:0]pc_bits;
 reg [4:0]ghr_bits;
 reg [4:0]pht_index;
+reg [5:0]inst_opcode1
+reg [5:0]inst_opcode2;
 
+inst_opcode1 = instruction[63:58];
+inst_opcode2 = instruction[31:26];
+
+//decode instruction to check if branch
+if (inst_opcode1 ==  BR_INST   || inst_opcode1 == BSR_INST ||  
+    inst_opcode1 ==  BLBC_INST || inst_opcode1 == BEQ_INST || 
+    inst_opcode1 ==  BLT_INST  || inst_opcode1 == BLE_INST || 
+    inst_opcode1 ==  BLBS_INST || inst_opcode1 == BNE_INST ||  
+    inst_opcode1 ==  BGE_INST  || inst_opcode1 == BGT_INST)
+  
+  isBranch = 1;
+else
+  isBranch = 0;
 
 //init pht to not taken. 0 means not taken, 1 means taken. 
 pht = 32'b0;
@@ -32,7 +48,10 @@ ghr_bits = {2'b0, ghr};
 
 pht_index = pc_bits^ghr_bits;
 
-prediction = pht[pht_index];
+if(inst_opcode1 == BR_INST || inst_opcode1 == BSR_INST)
+  prediction = 1;
+else
+  prediction = pht[pht_index];
 
 pht_index_out = pht_index;
 end
@@ -41,9 +60,11 @@ end
 always@
 ghr = ghr<<1;
 
-ghr[0] = prediction;
+//assume that unconditional branches don't count in the global branch history register (ghr)
+if (inst_opcode1 != BR_INST && inst_opcode1 != BSR_INST)
+  ghr[0] = prediction;
 
 if(result != pht[pht_index_in])
   pht[pht_index_in] = result;
 
-//assumption is that isBranch signal is just taken/not taken, and is the same thing as the prediction
+

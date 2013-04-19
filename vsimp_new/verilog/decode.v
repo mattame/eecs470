5fc7bc57054279d6e166a9fab69c7e19fb194d93
scottmel <scottmel@umich.edu>
2013-04-19 16:04:22 -0400

    decode fix again

--- vsimp_new/verilog/decode.v
+++ vsimp_new/verilog/decode.v
@@ -87,8 +87,8 @@ module decoder(// Inputs
           case (inst[31:26])
             `PAL_INST:
                if (inst[25:0] == 26'h0555) begin
-                  opa_select = 1;
-                  opb_select = 1;
+                  opa_select = 2;
+                  opb_select = 2;
                  halt = `TRUE;
                end
                else
@@ -304,8 +304,8 @@ module id_stage(
   output wire    [4:0] rb_idx_1;   // inst operand B register index
   wire    [4:0] rc_idx_1;     // inst operand C register index
   
-  assign ra_idx_1 = (id_opa_select_out_1 == `ALU_OPA_IS_REGA) ? if_id_IR_1[25:21] : `ZERO_REG;
-  assign rb_idx_1 = (id_opb_select_out_1 == `ALU_OPB_IS_REGB) ? if_id_IR_1[20:16] : `ZERO_REG;
+  assign ra_idx_1 = if_id_IR_1[25:21];
+  assign rb_idx_1 = if_id_IR_1[20:16];
   assign rc_idx_1 = if_id_IR_1[4:0];
 
     // instruction fields read from IF/ID pipeline register
@@ -313,8 +313,8 @@ module id_stage(
   output wire    [4:0] rb_idx_2;   // inst operand B register index
   wire           [4:0] rc_idx_2;     // inst operand C register index
 
-  assign ra_idx_2 = (id_opa_select_out_2 == `ALU_OPA_IS_REGA) ? if_id_IR_2[25:21]   : `ZERO_REG;
-  assign rb_idx_2 = (id_opb_select_out_2 == `ALU_OPB_IS_REGB) ? if_id_IR_2[20:16] : `ZERO_REG;
+  assign ra_idx_2 = if_id_IR_2[25:21];
+  assign rb_idx_2 = if_id_IR_2[20:16];
   assign rc_idx_2 = if_id_IR_2[4:0];
 
     // instantiate the instruction decoder

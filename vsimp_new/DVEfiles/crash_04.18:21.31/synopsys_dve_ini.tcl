c964cb171a8048262872940d8a5362caaa32c593
Matthew Nickolai <mattnick@dc3swp84.dc.umich.edu>
2013-04-19 01:36:09 -0400

    wat

---
+++ vsimp_new/DVEfiles/crash_04.18:21.31/synopsys_dve_ini.tcl
@@ -0,0 +1,72 @@
+gui_state_default_create -off -ini
+
+# Globals
+
+# Layout
+gui_set_state_value -category Layout -key child_console_size_x -value 1688
+gui_set_state_value -category Layout -key child_console_size_y -value 175
+gui_set_state_value -category Layout -key child_data_colvariable -value 192
+gui_set_state_value -category Layout -key child_data_size_x -value 329
+gui_set_state_value -category Layout -key child_data_size_y -value 777
+gui_set_state_value -category Layout -key child_hier_colhier -value 201
+gui_set_state_value -category Layout -key child_hier_coltype -value 107
+gui_set_state_value -category Layout -key child_hier_size_x -value 302
+gui_set_state_value -category Layout -key child_hier_size_y -value 777
+gui_set_state_value -category Layout -key child_schematic_docknewline -value false
+gui_set_state_value -category Layout -key child_schematic_pos_x -value {-2}
+gui_set_state_value -category Layout -key child_schematic_pos_y -value {-15}
+gui_set_state_value -category Layout -key child_schematic_size_x -value 1182
+gui_set_state_value -category Layout -key child_schematic_size_y -value 790
+gui_set_state_value -category Layout -key child_source_docknewline -value false
+gui_set_state_value -category Layout -key child_source_pos_x -value {-2}
+gui_set_state_value -category Layout -key child_source_pos_y -value {-15}
+gui_set_state_value -category Layout -key child_source_size_x -value 1060
+gui_set_state_value -category Layout -key child_source_size_y -value 772
+gui_set_state_value -category Layout -key child_wave_colname -value 233
+gui_set_state_value -category Layout -key child_wave_colvalue -value 233
+gui_set_state_value -category Layout -key child_wave_left -value 470
+gui_set_state_value -category Layout -key child_wave_right -value 1146
+gui_set_state_value -category Layout -key main_pos_x -value 32
+gui_set_state_value -category Layout -key main_pos_y -value 119
+gui_set_state_value -category Layout -key main_size_x -value 1720
+gui_set_state_value -category Layout -key main_size_y -value 1170
+gui_set_state_value -category Layout -key stand_wave_child_docknewline -value false
+gui_set_state_value -category Layout -key stand_wave_child_pos_x -value {-2}
+gui_set_state_value -category Layout -key stand_wave_child_pos_y -value {-15}
+gui_set_state_value -category Layout -key stand_wave_child_size_x -value 1626
+gui_set_state_value -category Layout -key stand_wave_child_size_y -value 849
+gui_set_state_value -category Layout -key stand_wave_top_pos_x -value 249
+gui_set_state_value -category Layout -key stand_wave_top_pos_y -value 217
+gui_set_state_value -category Layout -key stand_wave_top_size_x -value 1870
+gui_set_state_value -category Layout -key stand_wave_top_size_y -value 1170
+
+# list_value_column
+
+# Sim
+
+# Assertion
+
+# Stream
+
+# Data
+
+# Driver
+
+# Backtrace
+
+# Exclusion
+gui_set_state_value -category Exclusion -key favorite_exclude_annotation -value {}
+
+# SaveSession
+
+# FindDialog
+gui_create_state_key -category FindDialog -key m_pMatchCase -value_type bool -value false
+gui_create_state_key -category FindDialog -key m_pMatchWord -value_type bool -value false
+gui_create_state_key -category FindDialog -key m_pUseCombo -value_type string -value {}
+gui_create_state_key -category FindDialog -key m_pWrapAround -value_type bool -value true
+
+# Widget_History
+gui_create_state_key -category Widget_History -key TopLevel.1|qt_left_dock|DockWnd2|DLPane.1|pages|Data.1|hbox|textfilter -value_type string -value {*valid* *valid valid}
+
+
+gui_state_default_create -off

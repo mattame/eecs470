509c8edb3de0a2146a3d866c53abf8707b87d9e1
nowakmr <nowakmr@umich.edu>
2013-04-22 00:41:40 -0300

    Update nop.s

--- test/nop.s
+++ test/nop.s
@@ -1,7 +1,6 @@
 //test program with nops
-
-  
-  
+// this multiplies 2x10, puts it in r3, decrements until 0, breaks out, and  halts //
+    
       lda $r5, 10
       nop
       lda $r3, 2
@@ -14,5 +13,8 @@ loop:
       subq $r8, $r7, $r7
       nop
       bge $r7, loop
-      
+      nop
       addq $r3, $r3, $r3    
+      nop
+      call_pal 0x555
+      nop
